3b53af3d13d3e9d5eecdcb784e0e1c50c951d99f
darlenemaebulaon <mdmmbulaon.student@ua.edu.ph>
2024-09-23 17:51:01 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,75 +4,73 @@ function App() {
   return (
 <div class="resume">
     <header>
-      <h1>MILAGROS DARLENE MAE M. BULAON</h1>
-      <h2>FUTURE SOFTWARE ENGINEER</h2>
+      <h1>CEDRIC JIAN MARTIN</h1>
+      <h2>FUTURE BACK-END DEVELOPER</h2>
     </header>
 
     <div class="content">
       <section class="sidebar">
         <div class="contact">
           <h1>Contact</h1>
-            <p>mdmmbulaon.student@ua.edu.ph </p>
-            <p>09217233898  </p>
-            <p>San Luis, Pampanga </p>
-            <a href="https://github.com/darlenemaebulaon">Github </a> <br> <br>
-            <a href="https://ph.linkedin.com/in/milagros-darlene-mae-bulaon-3326a831a">LinkedIn </a>
+          <p><strong>Email:</strong> cjnmartin@email.com</p>
+          <p><strong>Facebook:</strong> Cedric Jian Martin</p>
+          <p><strong>Location:</strong> Sto.Tomas San matias Pampanga</p>
+
         </div>
 
         <div class="education">
           <h1>Education</h1>
-            <p>Bachelor of Science in Information Technology</p>
-            <p>University of the Assumption</p>
-            <p>(2022 - Present)</p>
+          <p>Bachelor of Science in Information Technology (Present)</p>
         </div>
 
         <div class="skills">
           <h1>Skills</h1>
-            <ul>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>JavaScript</li>
-              <li>Bootstrap</li>
-              <li>MySQL</li>
-              <li>Python</li>
-            </ul>
+          <ul>
+            <li>CSS</li>
+            <li>HTML</li>
+            <li>Python</li>
+            <li>MySQL</li>
+            <li>Bootstrap</li>
+            <li>JavaScript</li>
+
+          </ul>
         </div>
       </section>
 
       <section class="main-content">
         <div class="experience">
-          <h1>Learning Experience</h1>
-            <div class="yr">
-              <h3>First Year (2022 - 2023)</h3>
-                <h4><li>Python and Figma</li></h4>
-                  <ul>
-                    <p>- Learned the basics of Python, covering inputs, operations, sysntax, variables, functions, and loops.</p>
-                    <p>- Gained experience with Figma for designing wireframes, which enhanced understanding of UI/UX design.</p>
-                  </ul>
-            </div>
+          <h1>Experience Section</h1>
+          <h2>1st Year 2022-2023</h2>
+          <div class="yr">
+            <h4>Python and Figma</h4>
+            <p>Answer here</p>
+            <h3>Projects</h3>
+            <ul>
+              <li>.</li>
+            </ul>
+          </div>
 
-            <div class="yr">
-              <h3>Second Year (2023 - 2024)</h3>
-                <h4><li>MySQL, HTML, CSS, and JavaScript</li></h4>
-                  <ul>
-                    <p>- Studied programming languages like MySQL, HTML, CSS, and JavaScript.</p>
-                    <p>- Learned database creation, querying, and graph generation using MySQL.</p>
-                    <p>- Focused on web content structuring and styling with HTML and CSS, using CodePen to create interactive projects.</p>
-                  </ul>
-            </div>
+          <div class="yr">
+            <h2>2nd Year 2023-2024</h2>
+            <h4>Javascript, MySQL, HTML and CSS</h4>
+            <p>Answer here</p>
+            <h3>Projects</h3>
+            <ul>
+              <li>.</li>
+            </ul>
+          </div>
 
-            <div class="yr">
-              <h3>Third Year (2024 - Present)</h3>
-                <h4><li>HTML, CSS, JavaScript, Bootstrap, and REACTJS</li></h4>
-                  <ul>
-                    <p>- Focused on advanced HTML, CSS, JavaScript, Bootstrap, and React.js.</p>
-                    <p>- Developed dynamic websites and worked with contemporary tools like Vercel and Node.js.</p>
-                    <p>- Deployed projects on GitHub and replicated web designs to improve web development skills.</p>
-                  </ul>
-            </div>
+          <div class="yr">
+            <h1>3rd Year 2024 (Present) </h1>
+            <h4>HTML, CSS, REACTJS, and Bootstrap</h4>
+            <p>Answer here</p>
+            <h3>Projects</h3>
+            <ul>
+              <li>.</li>
+            </ul>
+          </div>
         </div>
       </section>
-
     </div>
   </div>
   );
